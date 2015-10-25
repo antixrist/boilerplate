58a6d5e18dbb48e0cc10315f4ff2cc1eba586e1e
antixrist <ya.antixrist@gmail.com>
2015-10-25 14:19:29 +0400

    Update jquery.boilerplate.js

--- jquery.boilerplate.js
+++ jquery.boilerplate.js
@@ -8,33 +8,33 @@
  */
 ;(function (window, document, $, undefined) {
 
-  var pluginName = "defaultPluginName";
+  var pluginName = 'defaultPluginName';
   var defaults = {
-      propertyName: "value"
-    };
+    propertyName: 'value'
+  };
 
-  function Plugin ( element, options ) {
+  var Plugin = function (element, options) {
     this.el = element;
     this.$el = $(element);
-    this.options = $.extend( {}, defaults, options );
+    this.options = $.extend(true, {}, defaults, options);
     this._defaults = defaults;
     this._name = pluginName;
     this.init();
-  }
+  };
 
   $.extend(Plugin.prototype, {
     init: function () {
 
     },
-    yourOtherFunction: function () {
+    otherFunction: function () {
 
     }
   });
 
   $.fn[pluginName] = function (options) {
     this.each(function () {
-      if (!$.data( this, "plugin_" + pluginName)) {
-        $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+      if (!$.data(this, 'plugin_'+ pluginName)) {
+        $.data(this, 'plugin_'+ pluginName, new Plugin(this, options));
       }
     });
 
